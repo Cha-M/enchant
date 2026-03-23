e73f223e5d0c5577367826eda7c45229c6c359da
Cha M <charlotte.megroff@outlook.com>
2026-03-23 06:38:26 +0000

    copy text of saved effects but there is an extra space where no magnitude, need to add attribute variants

--- src/data/effects.ts
+++ src/data/effects.ts
@@ -18,7 +18,7 @@ export const targetMultipliers: Record<string, number> = {
 };
 
 export const effects: Record<string, Effect> = {
-  // Add attribute variants
+  // Add attribute & skill variants
   "Absorb Attribute": {
     skill: "Mysticism",
     baseCost: 2,
