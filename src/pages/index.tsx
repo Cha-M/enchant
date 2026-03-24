b05fac443d32ba8b9e70906bcd85df36404571c7
Cha M <charlotte.megroff@outlook.com>
2026-03-24 20:38:13 +0000

    sort possible effects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -288,7 +288,7 @@ export default function Home() {
           }
 
           return true;
-        })
+        }).sort()
         .map((effectName) => (
           <option key={effectName} value={effectName}>
             {effectName}
